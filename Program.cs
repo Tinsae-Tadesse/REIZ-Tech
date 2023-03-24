a858e742cf50b25370e49518dee132414078de3f
Tinsae Tadesse <tinsaetadesse2015@gmail.com>
2023-03-24 19:45:16 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REIZ_Tech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
